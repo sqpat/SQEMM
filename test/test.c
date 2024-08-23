8c784cbcffe303aeb889fe9c9ab6f55723d0615d
patrick <pgoncalv17@gmail.com>
2024-08-23 16:00:17 -0400

    update some ems test code

--- test/test.c
+++ test/test.c
@@ -239,21 +239,77 @@ char EMSExists(void){
 
 
 // CleanUp() tries to deallocate our EMS allocation
-void FreeEMS()
-{
+void FreeEMS(){
+	int16_t errorreg;
    regs.h.ah=0x45;
    regs.w.dx=emshandle;
       intx86(EMS_INT, &regs, &regs);
-
-   if (regs.h.ah!=0) printf("Deallocation failed!\n");
+   errorreg =  regs.h.ah;
+   if (errorreg!=0) printf("Deallocation failed %x!\n", errorreg);
 }
  
 int16_t errorreg;
 int16_t numentries;
 
 int16_t pagenum9000;
-int16_t pagedata2[64];
+int16_t pageframe = 0;
+int16_t pagedata2[8];
 int16_t* far pointervalue2 = pagedata2;
+
+
+void mapAOld() {
+	int16_t i = 0;
+	int16_t j = 0;
+	int16_t k = 0;
+	//return;
+	for (i = 0; i < 4; i++){
+		regs.h.ah = 0x44;  // function
+		regs.h.al = pageframe+i;  // physical page
+		regs.w.bx = 64+i;  // logical page
+		regs.w.dx = emshandle; // handle
+		intx86(EMS_INT, &regs, &regs);
+		errorreg = regs.h.ah;
+		if (errorreg != 0) {
+			I_Error("Call 0x44 failed with value %x!\n", errorreg);
+		}
+
+		outp(0xe8, i + 4);
+		j = inp(0xea);
+		k = inp(0xeb);
+		printf("%i %x %x:", i, j, k);
+	}
+	printf("\n");
+}
+
+void mapBOld() {
+	int16_t i = 0;
+	int16_t j = 0;
+	int16_t k = 0;
+
+	//return;
+
+	for (i = 0; i < 4; i++){
+		regs.h.ah = 0x44;  // function
+		regs.h.al = pageframe+i;  // physical page
+		regs.w.bx = 60+i;  // logical page
+		regs.w.dx = emshandle; // handle
+		intx86(EMS_INT, &regs, &regs);
+		errorreg = regs.h.ah;
+		if (errorreg != 0) {
+			I_Error("Call 0x44 failed with value %x!\n", errorreg);
+		}
+
+		outp(0xe8, i + 4);
+		j = inp(0xea);
+		k = inp(0xeb);
+		printf("%i %x %x:", i, j, k);
+
+	}
+	printf("\n");
+
+}
+
+
 void mapA() {
 	pagedata2[0] = 4;
 	pagedata2[1] = pagenum9000;
@@ -306,7 +362,7 @@ void mapB() {
 }
 
 
-int I_InitEMS(void)
+void I_InitEMS(void)
 {
 	char	emmname[9] = "EMMXXXX0";
 	// todo check for device...
@@ -317,7 +373,7 @@ int I_InitEMS(void)
 	uint8_t vernum;
 	int16_t i, j;
 	//int16_t far *pagedata;
-	int16_t pagedata[64];
+	int16_t pagedata[80];
 	int16_t* far pointervalue = pagedata;
 
 
@@ -332,19 +388,7 @@ int I_InitEMS(void)
 		I_Error("Couldn't init EMS, error %x", errorreg);
 	}
 
-	printf("EMS exists...\n");
-	printf("Checking for EMS functionality...");
-
-
-	regs.h.ah = 0x40;
-	intx86(EMS_INT, &regs, &regs);
-	errorreg = regs.h.ah;
-	if (errorreg) {
-		I_Error("ems nonfunctional??? status %x\n", errorreg);
-	}
-
-	printf("EMS functional...\n");
-	printf("Checking EMS Version...\n");
+	printf("EMS functional... Checking EMS Version...\n");
 
 	regs.h.ah = 0x46;
 	intx86(EMS_INT, &regs, &regs);
@@ -361,7 +405,7 @@ int I_InitEMS(void)
 
 	}
 
-	printf("Getting page frame\n");
+	printf("Getting page frame: ");
 	// get page frame address
 	regs.h.ah = 0x41;
 	intx86(EMS_INT, &regs, &regs);
@@ -372,7 +416,7 @@ int I_InitEMS(void)
 	}
 
 	printf("Page frame was %x\n", pageframebase);
-	printf("Checking pages available\n");
+	printf("Checking pages available: ");
 
 
 	regs.h.ah = 0x42;
@@ -401,12 +445,12 @@ int I_InitEMS(void)
 		I_Error("Couldn't allocate %d EMS Pages, error %x", numPagesToAllocate, regs.h.ah);
 	}
 
-	printf("found handle %i", emshandle);
+	printf("found handle %i ", emshandle);
 
 
 	// do initial page remapping
 
-
+	/*
 	for (j = 0; j < 4; j++) {
 		regs.h.al = j;  // physical page
 		regs.w.bx = j;    // logical page
@@ -418,7 +462,7 @@ int I_InitEMS(void)
 			I_Error("Mapping failed (%x) on page %i ! ", errorreg ,j);
 		}
 	}
-
+	*/
 
 
 	regs.w.ax = 0x5801;  // physical page
@@ -428,7 +472,7 @@ int I_InitEMS(void)
 	if (errorreg != 0) {
 		I_Error("Call 5801 failed with value %x!\n", errorreg);
 	}
-	printf("Call 5801 found: %i\n", numentries);
+	printf("Total frame count: %i\n", numentries);
 
 
 	regs.w.ax = 0x5800;  // physical page
@@ -440,7 +484,10 @@ int I_InitEMS(void)
 	if (errorreg != 0) {
 		I_Error("Call 25 failed with value %x!\n", errorreg);
 	}
-	printf("Call 25 found:\n%x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x\n%x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x\n%x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x\n%x %x %x %x %x %x %x %x\n",
+
+//	printf("Call 25 found:\n%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n",
+
+	printf("Call 25 found:\n%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t\n%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t\n%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t\n%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t%x %x\t\n%x %x\t%x %x\t%x %x\t%x %x\n",
 		pagedata[0], pagedata[1], pagedata[2], pagedata[3], pagedata[4],
 		pagedata[5], pagedata[6], pagedata[7], pagedata[8], pagedata[9],
 		pagedata[10], pagedata[11], pagedata[12], pagedata[13], pagedata[14],
@@ -452,7 +499,10 @@ int I_InitEMS(void)
 		pagedata[40], pagedata[41], pagedata[42], pagedata[43], pagedata[44],
 		pagedata[45], pagedata[46], pagedata[47], pagedata[48], pagedata[49],
 		pagedata[50], pagedata[51], pagedata[52], pagedata[53], pagedata[54],
-		pagedata[55]//, pagedata[56], pagedata[57], pagedata[58], pagedata[59],
+		pagedata[55], pagedata[56], pagedata[57], pagedata[58], pagedata[59],
+		pagedata[60], pagedata[61], pagedata[62], pagedata[63], pagedata[64],
+		pagedata[65], pagedata[66], pagedata[67], pagedata[68], pagedata[69],
+		pagedata[70], pagedata[71]
 
 	);
 
@@ -461,72 +511,80 @@ int I_InitEMS(void)
 	for (i = 0; i < numentries; i ++) {
 		if (pagedata[i*2] == 0x9000u) {
 			pagenum9000 = pagedata[(i*2) + 1];
-			printf("\Pagenum for 0x9000 was: %i", pagenum9000);
-			goto found;
+			printf( "Pagenum for 0x9000 was: %x", pagenum9000);
 		}
+		if (pagedata[i*2] == pageframebase) {
+			pageframe = pagedata[(i*2) + 1];
+			printf(" Pagenum for %x was: %i", pageframebase, pageframe);
+			
+		}
+
 	}
 
-	printf("\nPagenum for 0x9000 NOT FOUND!");
 
-	found:
+	//printf("\nPagenum for 0x9000 NOT FOUND!");
+
+	//found:
 
 
 	// initial setup, map seg 9000 memory to 4-7
 	// cant do 0-3 cause up above we mapped that to page frame
-
-	mapA();
+	printf("\n");
+	mapAOld();
 
 	// write data
 
-	fakepointer = MK_FP(0x9000, 0x0000);
+	fakepointer = MK_FP(0xD000, 0x0000);
 	*fakepointer = 0x1234;
-	fakepointer = MK_FP(0x9400, 0x0000);
+	fakepointer = MK_FP(0xD400, 0x0000);
 	*fakepointer = 0x3456;
-	fakepointer = MK_FP(0x9800, 0x0000);
+
+	fakepointer = MK_FP(0xD800, 0x0000);
 	*fakepointer = 0x5678;
-	fakepointer = MK_FP(0x9c00, 0x0000);
+	fakepointer = MK_FP(0xDC00, 0x0000);
 	*fakepointer = 0x789A;
 
 	//page out, by paging in 60-63
 
-	mapB();
+	mapBOld();
 
 
 
-	fakepointer = MK_FP(0x9000, 0x0000);
+	fakepointer = MK_FP(0xD000, 0x0000);
 	*fakepointer = 0xAAAA;
-	fakepointer = MK_FP(0x9400, 0x0000);
+	fakepointer = MK_FP(0xD400, 0x0000);
 	*fakepointer = 0xBBBB;
-	fakepointer = MK_FP(0x9800, 0x0000);
+	fakepointer = MK_FP(0xD800, 0x0000);
 	*fakepointer = 0xCCCC;
-	fakepointer = MK_FP(0x9c00, 0x0000);
+	fakepointer = MK_FP(0xDC00, 0x0000);
 	*fakepointer = 0xDDDD;
 	 
-	mapA();
+	mapAOld();
 
 
 	printf("\nvalues were:");
-	fakepointer = MK_FP(0x9000, 0x0000);
+	fakepointer = MK_FP(0xD000, 0x0000);
 	printf("%x ", *fakepointer);
-	fakepointer = MK_FP(0x9400, 0x0000);
+	fakepointer = MK_FP(0xD400, 0x0000);
 	printf("%x ", *fakepointer);
-	fakepointer = MK_FP(0x9800, 0x0000);
+	fakepointer = MK_FP(0xD800, 0x0000);
 	printf("%x ", *fakepointer);
-	fakepointer = MK_FP(0x9c00, 0x0000);
+	fakepointer = MK_FP(0xDc00, 0x0000);
 	printf("%x ", *fakepointer);
 	printf("\n");
 
-	mapB();
+	mapBOld();
+	
 	printf("\nother values were:");
-	fakepointer = MK_FP(0x9000, 0x0000);
+	fakepointer = MK_FP(0xD000, 0x0000);
 	printf("%x ", *fakepointer);
-	fakepointer = MK_FP(0x9400, 0x0000);
+	fakepointer = MK_FP(0xD400, 0x0000);
 	printf("%x ", *fakepointer);
-	fakepointer = MK_FP(0x9800, 0x0000);
+	fakepointer = MK_FP(0xD800, 0x0000);
 	printf("%x ", *fakepointer);
-	fakepointer = MK_FP(0x9c00, 0x0000);
+	fakepointer = MK_FP(0xDc00, 0x0000);
 	printf("%x ", *fakepointer);
-	printf("\n");
+	
 	/*
 	1. try dosbox-x
 
@@ -541,7 +599,7 @@ int I_InitEMS(void)
 
 
     // EMS Handle
-    return emshandle;
+    //return emshandle;
 
  
    
@@ -552,21 +610,15 @@ int I_InitEMS(void)
 
 int main(void)
   {
-        uint16_t far* addr;
 		int16_t result;
 
-		int handle = I_InitEMS();
+		I_InitEMS();
  
 
-        printf("return handle was %i corresponding to addr of %lu\n", handle, addr);
-
-
-		addr = (uint16_t far *) MK_FP(pageframebase, 0);
-		printf("return handle was %i corresponding to addr of %lu\n", handle, addr);
-
-		printf("freeing ems handle %i\n", handle);
-		FreeEMS(handle);
-		printf("ems freed");
+		printf("\nreturn handle was %i corresponding to addr of %Fp\n", emshandle, MK_FP(pageframebase, 0));
+		printf("freeing ems handle %i\n", emshandle);
+		FreeEMS();
+		printf("ems freed successfully");
 
 
         return 0;
